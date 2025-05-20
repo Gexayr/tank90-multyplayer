7fb131e09d6be3fadc64d469dc5e207e8c0a83cd
mgexayr <mgexayr@smartbet.llc>
2025-05-20 15:20:51 +0400

    add get server url from env file

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONT_URI || "http://localhost:5173",
     methods: ["GET", "POST"]
   }
 });
